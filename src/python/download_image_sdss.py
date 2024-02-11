b0feaf3f1bc2381427df2eb3b840d137bb7600a5
Jean Gomes <neutrinomuon@gmail.com>
2024-02-11 13:24:08 +0000

    Changed some bugs in the importing library

--- src/python/download_image_sdss.py
+++ src/python/download_image_sdss.py
@@ -15,6 +15,7 @@ Written: Jean Michel Gomes © Copyright
 """
 import os
 import sys
+import re
 
 # import subprocess
 from collections import OrderedDict
@@ -33,13 +34,16 @@ def print_welcome_message():
     print("####################################################")
     print()
 
-def print_how_to_use_it():
+def print_how_to_use_it(error=''):
     '''Print "How to Use it?" instructions'''
     print("####################################################")
     print("#     Version 01 -- SDSS DR18 - How to use it?     #")
     print("#            Script to get SDSS images             #")
     print("#   Needs a file with objects, RA & DEC columns!   #")
     print("####################################################")
+
+    print()
+    print(f"Error in commands: {error}")
 
     print("\n####################################################")
     print("Usage:")
@@ -87,12 +91,14 @@ def define_sdss_link(**kwargs):
     data_release_lower = kwargs['data_release'].lower()
     data_release_upper = kwargs['data_release'].upper()
 
+    # Debug
+    # print("opt ============> ",kwargs['opt'])
     if data_release_lower in ('dr7','dr8','dr9','dr10'):
         link = (
             f"http://skyservice.pha.jhu.edu/{data_release_lower.capitalize()}/"
             f"ImgCutout/getjpeg.aspx?ra={kwargs['ra_value']}&dec={kwargs['decvalue']}&"
             f"scale={kwargs['scale']}&width={kwargs['width']}&height={kwargs['height']}&"
-            "opt={kwargs['opt']}"
+            f"opt={kwargs['opt']}"
         )
     elif data_release_upper in ('DR12','DR13','DR14','DR15','DR16','DR17','DR18'):
         link = (
@@ -105,6 +111,8 @@ def define_sdss_link(**kwargs):
         print(f"Unsupported SDSS Data Release: {kwargs['data_release']}")
         link = ''
 
+    # Debug
+    # print(link)
     return link
 
 def download_sdss_image(galaxy_name, **kwargs):
@@ -130,6 +138,9 @@ def download_sdss_image(galaxy_name, **kwargs):
     # kwargs['data_release'] = kwargs['data_release'].upper()
     # data_release_upper = kwargs['data_release']
 
+    # Debug
+    # print()
+    # print("opt ===> ",kwargs['opt'])
     link = define_sdss_link(ra_value=kwargs['ra_value'],
                             decvalue=kwargs['decvalue'],
                             plateid=kwargs['plateid'],
@@ -222,9 +233,11 @@ def check_main_options(width=600,
                        default_opts='',
                        create_link_flag=False):
     '''Check Main options'''
+    ierror = False
     valid_letters = "GLPSTOBFMQI"
     raw_opt = sys.argv[2] if len(sys.argv) > 2 else default_opts
     # raw_opt = default_opts
+    # print(raw_opt)
 
     if not all(char in valid_letters for char in raw_opt):
         print(f"WARNING: Invalid value for SDSS options. Only the letters \
@@ -236,65 +249,125 @@ GLPSTOBFMQI are allowed, but {raw_opt} used.")
     else:
         opt = raw_opt
 
-    scale  = float(sys.argv[3]) if len(sys.argv) > 3 else scale
-    if scale <= 0:
-        print("WARNING: Invalid value for scale. Scale must be greater than 0.0.")
-        scale = 0.1
+    try:
+        scale = float(sys.argv[3]) if len(sys.argv) > 3 else scale
+    except ValueError:
+        scale  = sys.argv[3]
+    # print(scale)
+    if not isinstance(scale, float) or float(scale) <= 0.0:
+        error = f'scale == {scale}' \
+              + ' ==> Should be float > 0.0!'
+        print_how_to_use_it(error)
+        ierror = True
+        return opt,width,height,scale,create_link_flag,ierror
+    # if scale <= 0:
+    #     print("WARNING: Invalid value for scale. Scale must be greater than 0.")
+    #     scale = 0.1
 
-    width  = int(sys.argv[4]) if len(sys.argv) > 4 else width
-    if width <= 0:
-        print("WARNING: Invalid value for width. Width must be greater than 0.")
-        width = 600
+    try:
+        width = int(sys.argv[4]) if len(sys.argv) > 4 else width
+    except ValueError:
+        # print('Error value')
+        width = sys.argv[4]
+    if not isinstance(width, int) or int(width) < 1:
+        error = f'width == {width}' \
+              + ' ==> Should be integer > 1!'
+        print_how_to_use_it(error)
+        ierror = True
+        return opt,width,height,scale,create_link_flag,ierror
+    # if width <= 0:
+    #     print("WARNING: Invalid value for width. Width must be greater than 0.")
+    #     width = 600
+    # print(width,type(width))
 
-    height = int(sys.argv[5]) if len(sys.argv) > 5 else height
-    if height <= 0:
-        print("WARNING: Invalid value for height. Height must be greater than 0.")
-        height = 600
+    try:
+        height = int(sys.argv[5]) if len(sys.argv) > 5 else height
+    except ValueError:
+        # print('Error value')
+        height = sys.argv[5]
+    if not isinstance(height, int) or int(height) < 1:
+        error = f'height == {height}' \
+              + ' ==> Should be integer > 1!'
+        print_how_to_use_it(error)
+        ierror = True
+        return opt,width,height,scale,create_link_flag,ierror
+    # if height <= 0:
+    #     print("WARNING: Invalid value for height. Height must be greater than 0.")
+    #     height = 600
 
     # Assuming the sixth argument is the flag to create the symbolic link
-    create_link_flag = bool(sys.argv[6]) if len(sys.argv) > 6 else False
+    # Check if the string represents a boolean value
+    create_link_flag = sys.argv[7].lower() if len(sys.argv) > 3 else create_link_flag
+    if create_link_flag.lower() == 'true' or create_link_flag is True:
+        create_link_flag = True
+    elif create_link_flag.lower() == 'false' or create_link_flag is False:
+        create_link_flag = False
+    else:
+        error = f'create_link_flag == {create_link_flag}' \
+              + ' ==> Should be boolean - True or False!'
+        print_how_to_use_it(error)
+        ierror = True
+        return opt,width,height,scale,create_link_flag,ierror
+    # create_link_flag = False
+    # create_link_flag = bool_value if len(sys.argv) > 6 else False
+    # print(create_link_flag)
 
-    return opt,width,height,scale,create_link_flag
+    # if not isinstance(create_link_flag, bool):
+    #     error = f'create_link_flag == {create_link_flag}' \
+    #           + ' ==> Should be bool value'
+    #     print_how_to_use_it(error)
+    #     ierror = True
+    #     return opt,width,height,scale,create_link_flag,ierror
+
+    return opt,width,height,scale,create_link_flag,ierror
 
 def verify_data_release(data_release):
     '''Verify SDSS data release, if available'''
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 2: # In case sys.argv - command line is used!
         data_release_ = str(sys.argv[-1]).lower()
 
-        if data_release_ in ('dr7',
-                             'dr8',
-                             'dr9',
-                             'dr10',
-                             'dr12',
-                             'dr13',
-                             'dr14',
-                             'dr15',
-                             'dr16',
-                             'dr17',
-                             'dr18'):
-            data_release = data_release_
-            # Reduce the number of arguments in sys.argv
-            sys.argv = sys.argv[:-1]
+        if data_release_[0:2] == 'dr':
+            if data_release_ in ('dr7',
+                                 'dr8',
+                                 'dr9',
+                                 'dr10',
+                                 'dr12',
+                                 'dr13',
+                                 'dr14',
+                                 'dr15',
+                                 'dr16',
+                                 'dr17',
+                                 'dr18'):
+                data_release = data_release_
+                # Reduce the number of arguments in sys.argv
+                sys.argv = sys.argv[:-1]
+            else:
+                print("Nothing")
+                data_release = 'dr18'
         else:
             if data_release_[0:2] == 'dr':
                 print(f"Unsupported SDSS Data Release: {data_release_.upper()}")
                 # Reduce the number of arguments in sys.argv
                 sys.argv = sys.argv[:-1]
-            data_release = 'dr18'
-    else:
+                data_release_ = str(sys.argv[-1]).lower() #'dr18'
+
+    else: # In case command line is not used!
         data_release_ = data_release.lower()
-        if data_release_ in ('dr7',
-                             'dr8',
-                             'dr9',
-                             'dr10',
-                             'dr12',
-                             'dr13',
-                             'dr14',
-                             'dr15',
-                             'dr16',
-                             'dr17',
-                             'dr18'):
-            data_release = data_release_
+        if data_release_[0:2] == 'dr':
+            if data_release_ in ('dr7',
+                                 'dr8',
+                                 'dr9',
+                                 'dr10',
+                                 'dr12',
+                                 'dr13',
+                                 'dr14',
+                                 'dr15',
+                                 'dr16',
+                                 'dr17',
+                                 'dr18'):
+                data_release = data_release_
+            else:
+                data_release = 'dr18'
         else:
             if data_release_[0:2] == 'dr':
                 print(f"Unsupported SDSS Data Release: {data_release_.upper()}")
@@ -302,57 +375,80 @@ def verify_data_release(data_release):
 
     return data_release
 
+def is_valid_filename(filename):
+    '''Check if filename is valid'''
+    # Check if the filename is not empty and does not contain any illegal characters
+    return bool(filename and re.match(r'^[^\\/:*?"<>|]+$', filename))
+
+def check_irerror(ierror):
+    '''Check for errors in reading the parameters'''
+    if ierror:
+        sys.exit()
+
 def main(filename=None,
-         data_release='dr18',
-         default_opts='',
-         create_link_flag=False,
          **kwargs):
     '''Main function for downloading a list of objects from SDSS'''
     # print(filename)
     # print("sys????? ==== ",sys.argv[:])
 
     # Access dictionary
-    kwargs['scale'] = kwargs.get('scale', 0.1)
-    kwargs['width'] = kwargs.get('width', 600)
+    kwargs['scale']  = kwargs.get('scale', 0.1)
+    kwargs['width']  = kwargs.get('width', 600)
     kwargs['height'] = kwargs.get('height', 600)
+    kwargs['create_link_flag'] = kwargs.get('create_link_flag',False)
+    kwargs['data_release']  = kwargs.get('data_release', 'dr18')
+    kwargs['default_opts']  = kwargs.get('default_opts', '')
 
-    if filename is None:
+    if filename is None and len(sys.argv) > 1:
         # If called without a filename, use sys.argv
-        # print(filename)
-        if len(sys.argv) > 1:
-            filename = sys.argv[1]
-        else:
-            print_how_to_use_it()
+        filename = sys.argv[1]
+        if not os.path.exists(filename):
+            print("... File does not exist:", filename)
+            print_how_to_use_it(filename)
+            return
+    else:
+        if not isinstance(filename, str):
+            print_how_to_use_it(filename)
+            return
+        if not os.path.exists(filename):
+            print("... File does not exist:", filename)
+            print_how_to_use_it(filename)
             return
 
     print_welcome_message()
-    verify_data_release(data_release)
+    kwargs['data_release'] = verify_data_release(kwargs['data_release'])
 
-    kwargs['data_release'] = data_release
-    kwargs['create_link_flag'] = create_link_flag
+    # kwargs['data_release'] = data_release
+    # kwargs['create_link_flag'] = create_link_flag
 
     # Read file
     with open(filename, 'r', encoding='utf-8') as file:
         lines = [line.strip() for line in file if not line.startswith('#')]
 
-    print(f'We are using SDSS Data Release: {data_release.upper()}')
+    print(f"We are using SDSS Data Release: {kwargs['data_release'].upper()}")
+
     # numb_lines = len(lines)
 
     # Define arguments
     # valid_letters = "GLPSTOBFMQI"
 
     # print(kwargs['data_release'])
+    # print(kwargs['scale'])
     kwargs['opt'], \
     kwargs['width'], \
     kwargs['height'], \
     kwargs['scale'], \
-    kwargs['create_link_flag'] = check_main_options(
+    kwargs['create_link_flag'], \
+        ierror = check_main_options(
         width=kwargs['width'],
         height=kwargs['height'],
         scale=kwargs['scale'],
-        default_opts=default_opts,
+        default_opts=kwargs['default_opts'],
         create_link_flag=kwargs['create_link_flag']
     )
+
+    # check_ierror
+    check_irerror(ierror)
 
     # Create a progress bar for the overall download process
     total_progress_bar = tqdm(total=len(lines),
@@ -404,7 +500,7 @@ def main(filename=None,
                             width=kwargs['width'],
                             height=kwargs['height'],
                             create_link_flag=kwargs['create_link_flag'],
-                            data_release=data_release,
+                            data_release=kwargs['data_release'],
                             progress=False)
 
         total_progress_bar.update(1)
